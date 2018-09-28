d7c3fb98926283f0e3f9354ea7b0337e58ba5f1e
YQRY <2991485737@qq.com>
2018-09-28 16:44:57 +0800

    Initial commit

---
+++ wordgame1.py
@@ -0,0 +1,14 @@
+# users/朔/PycharmProjects
+# -*- coding:utf-8 -*-
+# author:YQRY time:2018/9/28
+print("猜数小游戏")
+temp = input("猜我想的是哪个数：")
+guess = int(temp)
+if guess == 8:
+    print("牛逼！猜对了！")
+else:
+    if guess > 8:
+        print("大了")
+    else:
+        print("小了")
+print("游戏结束！")
