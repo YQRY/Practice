ec4e56f93480491bfa52b982a722a5ce88094fdd
YQRY <2991485737@qq.com>
2018-10-08 21:36:00 +0800

    cs

---
+++ 100以内偶数.py
@@ -0,0 +1,10 @@
+# users/朔/PycharmProjects
+# -*- coding:utf-8 -*-
+# author:YQRY time:2018/10/8
+i = 0
+while i <= 100:
+    if i%2 != 0:
+        i += 1
+        print(i,end=' ')
+    else:
+        i += 1
