2387c1df039839aabbf347934defb2cce6a2f097
YQRY <2991485737@qq.com>
2018-10-11 18:47:24 +0800

    分数等级换算

---
+++ 分数等级换算.py
@@ -0,0 +1,11 @@
+score = int(input('请输入分数：'))
+if 90 <= score <= 100:
+    print('A')
+elif 80 <= score <90:
+    print('B')
+elif 60 <= score <80:
+    print('C')
+elif 0 <= score <60:
+    print('D')
+else:
+    print('输入错误！')
