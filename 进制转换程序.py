738f116f34e588af07f60a738c00ff2b5a2f2969
YQRY <2991485737@qq.com>
2018-10-25 17:16:04 +0800

    进制转换程序

---
+++ 进制转换程序.py
@@ -0,0 +1,13 @@
+# users/朔/PycharmProjects
+# -*- coding:utf-8 -*-
+# author:YQRY time:2018/10/23
+q = True
+while q:
+    num = input('请输入一个整数（输入Q结束程序）：')
+    if num != 'Q':
+        num = int(num)
+        print('十进制 -> 十六进制 : %d -> 0x%x' % (num,num))
+        print('十进制 -> 八进制 : %d -> 0o%x' % (num,num))
+        print('十进制 -> 二进制 : %d -> ' % num, bin(num))
+    else:
+        q = False
